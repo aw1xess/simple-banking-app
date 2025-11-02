40cdc6c5f1502bb5b2e021fdd629305461d19c70
aw1xess <yariksvirshchuk@gmail.com>
2025-11-02 12:47:59 +0200

    Fixed issue

--- lib/auth/authUtils.ts
+++ lib/auth/authUtils.ts
@@ -2,12 +2,12 @@ export const rpName = "Simple Banking App";
 
 export const rpID =
   process.env.NODE_ENV === "production"
-    ? "your-production-domain.com" // ⚠️ Замініть на ваш домен
+    ? "simple-banking-app-iota.vercel.app" // ⚠️ Замініть на ваш домен
     : // : "fishy-nonproblematically-craig.ngrok-free.dev";
       "localhost";
 
 export const rpOrigin =
   process.env.NODE_ENV === "production"
-    ? "https://your-production-domain.com" // ⚠️ Замініть на ваш домен
+    ? "https://simple-banking-app-iota.vercel.app" // ⚠️ Замініть на ваш домен
     : // : "https://fishy-nonproblematically-craig.ngrok-free.dev";
       "http://localhost:3000";
